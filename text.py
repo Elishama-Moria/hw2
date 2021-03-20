30397e98af6fc51bc8bd736e16ebd9053cd5609e
Elishama-Moria <80213152+Elishama-Moria@users.noreply.github.com>
2021-03-20 21:20:13 +0200

    Add files via upload

---
+++ text.py
@@ -0,0 +1,19 @@
+def revword(word:str) -> str:
+    word=word.lower()[::-1]
+    return word
+
+def countword() -> int:
+    my_text_opened = open('text.txt', 'r')
+    my_text = my_text_opened.read()
+    space_text = my_text.replace('\n', ' ')
+    text_list = space_text.split(' ')
+    word = text_list[0]
+    my_word_cnt=0
+    for a_word in text_list:
+        if my_word_cnt==0:
+            my_word_cnt+=1
+        else:
+            a_word=revword(a_word)
+            if a_word==word:
+                my_word_cnt+=1
+    return my_word_cnt
