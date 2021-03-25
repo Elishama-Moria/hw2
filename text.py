aff1f1874d73e7d4e5174ab338a46efde8390682
Elishama-Moria <80213152+Elishama-Moria@users.noreply.github.com>
2021-03-25 11:30:33 +0200

    Update text.py

--- text.py
+++ text.py
@@ -7,7 +7,7 @@ def countword() -> int:
     my_text = my_text_opened.read()
     space_text = my_text.replace('\n', ' ')
     text_list = space_text.split(' ')
-    word = text_list[0]
+    word = text_list[0].lower()
     my_word_cnt=0
     for a_word in text_list:
         if my_word_cnt==0:
